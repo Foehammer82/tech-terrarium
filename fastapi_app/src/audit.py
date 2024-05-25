b43315c3ab47f6a5629f1787bca07ea2555d80dc
foehammer <blakej42@live.com>
2024-05-25 13:06:10 -0500

    got an idea for how to handle the audit failed checker engine working both async and sync.

--- fastapi_app/src/audit.py
+++ fastapi_app/src/audit.py
@@ -1,6 +1,8 @@
-import abc
+import asyncio
+import inspect
 import threading
-from typing import Dict, Any, Optional
+from enum import StrEnum
+from typing import Dict, Any, Optional, Union, Literal
 from uuid import UUID, uuid4
 
 import orjson
@@ -9,10 +11,11 @@ from confluent_kafka import Producer
 from confluent_kafka.avro import SerializerError
 from confluent_kafka.schema_registry import SchemaRegistryClient, SchemaRegistryError
 from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.schema_registry.json_schema import JSONSerializer
 from confluent_kafka.serialization import SerializationContext, MessageField
 from fastapi import FastAPI
 from loguru import logger
-from pydantic import Field, BaseModel, model_validator, PrivateAttr
+from pydantic import Field, BaseModel
 from starlette.concurrency import iterate_in_threadpool
 from starlette.requests import Request
 
@@ -25,80 +28,38 @@ from starlette.requests import Request
 # console_handler.setLevel(logging.DEBUG)
 # logger.addHandler(console_handler)
 
-# TODO: make it optional to use avro or json serialization and switch to json.
-#       - lesson learned: avro may be fast, but is not very flexible and may end up creating more challenges down the
-#         road for auditing.  since we end up using json in avro for nested dicts anyway.  Avro is great if for simple
-#         data structures, but for more complex data structures, it may be better to just use json.  even with the loss
-#         of speed.  it might be nice to use this to build the starlette middleware to use avro and the default
-#         auditing to use json, and possibly move away from subclassing BaseModel to AuditBaseModel and just use
-#         BaseModel.  then have the BaseModel checked for serialization at start of auditing.  I also just need this
-#         to work soon so i can move onto the rest of standing up the terrarium.
 
-
-class AuditBaseModel(BaseModel, abc.ABC):
-    # TODO: add validation check for no Tuple's or Unions.
-    # TODO: review and make sure all types adhere to Avro types
-    #       https://avro.apache.org/docs/1.11.1/specification/#primitive-types
-
-    _audit_uuid = PrivateAttr(default_factory=uuid4)
-    _avro_schema: str = None
-
-    @property
-    def audit_uuid(self):
-        """
-        The UUID of the audit model.
-
-        Notes:
-            We use a property here to ensure that the UUID is not `easily` edited during the object lifecycle. and
-            so that the UUID is not passed to the json output of the pydantic model.  This enables it to be used
-            programmatically, but not be appended unexpectedly to the json output.
-        """
-        return self._audit_uuid
-
-    @classmethod
-    def get_avro_schema(cls):
-        return py_avro_schema.generate(cls).decode()
-
-    def avro_dict(self):
-        dict = self.dict()
-
-        if all([type(k) is str for k in dict.items()]):
-
-
-        return {k: v if (type(v) is Dict[str, Any]) else orjson.dumps(v) for k, v in self.dict().items()}
-
-    @model_validator(mode="before")
-    @classmethod
-    def check_avro_schema(cls, data: Any) -> Any:
-        try:
-            cls._avro_schema = py_avro_schema.generate(cls).decode()
-        except Exception as e:
-            raise ValueError(f"Failed to generate Avro schema for {cls.__name__}: {e}") from e
-
-        return data
+class SerializerClass(StrEnum):
+    AVRO = "avro"
+    JSON = "json"
 
 
 class Auditor:
     # TODO: configure a timer or scheduler to reprocess audit messages on the retry-topic
     _kafka_dead_letter_producer: Optional[Producer] = None
+    _schema_registry_models: dict[BaseModel.__class__.__name__, AvroSerializer] = {}
 
     def __init__(
         self,
         kafka_brokers: str | list[str],
         schema_registry_url: str,
-        schema_registry_default_models: list[AuditBaseModel] = None,
         retry_topic: str | None = None,
         dead_letter_topic: str | None = None,
+        default_serializer: Literal["json", "avro"] = "json",
     ):
-        # store the Avro serializers for the default models in the schema registry
-        self._schema_registry_url = schema_registry_url
-        self._schema_registry_models: dict[AuditBaseModel.__class__.__name__, AvroSerializer] = (
-            {model.__class__.__name__: self._get_avro_serializer(model) for model in schema_registry_default_models}
-            if schema_registry_default_models
-            else {}
-        )
+        """
+        Initializes the Auditor.
 
-        # Initialize the main Kafka producer
+        Args:
+            kafka_brokers: Kafka broker(s) to connect to.
+            schema_registry_url: URL of the Schema Registry.
+            retry_topic: Topic name to send messages that fail to be delivered.
+            dead_letter_topic: Topic name to send messages that fail to be delivered after retrying.
+            default_serializer: ('json', 'avro') Default serializer to use for models that do not have a specific
+                                serializer.
+        """
+        self._default_serializer = default_serializer
+        self._schema_registry_url = schema_registry_url
         self._kafka_producer = Producer({"bootstrap.servers": kafka_brokers})
 
         # Initialize the retry producer if a retry topic is provided
@@ -117,43 +78,73 @@ class Auditor:
         else:
             logger.warning("No dead-letter topic configured, messages that fail to be delivered will be lost.")
 
-    def add_model_to_schema_registry(self, model: AuditBaseModel):
-        if isinstance(model, AuditBaseModel):
-            self._schema_registry_models[model.__class__.__name__] = self._get_avro_serializer(model.__class__)
-        else:
-            self._schema_registry_models[model.__name__] = self._get_avro_serializer(model)
+        try:
+            self.task = asyncio.create_task(self._background_task())
+        except RuntimeError:
+            logger.info("No running event loop found, running background task.  Starting a new event loop")
+            asyncio.run(self._background_task())
 
-    def audit(self, audit_data: AuditBaseModel, kafka_topic: str):
+    async def _background_task(self):
+        # TODO: have this work by checking if this is running when audit is called, then adding it as a task to the
+        #       active event loop.  if no event loop is found, then run the audit synchronously blocking until the
+        #       audit is complete.  (make sure to log a warning if this is being run synchronously to let the user know
+        #       that audit isn't running as efficiently as it could be)
+
+        while True:
+            logger.info(f"background task running in thread: {threading.current_thread()}")
+            yield
+            await asyncio.sleep(5)
+
+    def add_model_to_schema_registry(self, model: BaseModel, serializer_class: SerializerClass = None):
+        if serializer_class is None:
+            serializer_class = self._default_serializer
+
+        if isinstance(model, BaseModel):
+            self._schema_registry_models[model.__class__.__name__] = self._get_serializer(
+                model.__class__, serializer_class
+            )
+        elif inspect.isclass(model) and issubclass(model, BaseModel):
+            self._schema_registry_models[model.__name__] = self._get_serializer(model, serializer_class)
+        else:
+            raise ValueError("model must be an instance of BaseModel or a BaseModel subclass.")
+
+    def audit(self, audit_data: BaseModel, kafka_topic: str, serializer_class: SerializerClass = None):
         logger.debug(f"Current Thread: {threading.current_thread()}")
 
-        if not isinstance(audit_data, AuditBaseModel):
-            raise ValueError("audit_data must be an instance of AuditBaseModel")
+        if serializer_class is None:
+            serializer_class = self._default_serializer
+
+        if not isinstance(audit_data, BaseModel):
+            raise ValueError("audit_data must be an instance of BaseModel")
 
         # TODO: CONSIDERATION: need to setup some rigorous testing on this to see how it handles being hit heavily from
         #                      async calls (will also want to adapt this to run async as well)  might need to setup a
         #                      queue, or maybe just need to produce asynchrously and let the kafka producer handle the
         #                      rest.  take a look at: https://www.confluent.io/blog/kafka-python-asyncio-integration/
+        # TODO: there is no way to know if the audit was successful or not, we need to add some sort of async checker
+        #       or something to check on the audit threads once and a while to see if they passed or failed, and then
+        #       raise errors in the main thread if they failed and raised errors of their own.
         # run the audit in a separate thread, so we can get back to the task at hand ASAP
-        audit_request_thread = threading.Thread(target=self._do_audit, args=[audit_data, kafka_topic])
+        audit_request_thread = threading.Thread(target=self._do_audit, args=[audit_data, kafka_topic, serializer_class])
         audit_request_thread.start()
 
-    def _do_audit(self, audit_data: AuditBaseModel, kafka_topic: str):
+    def _do_audit(self, audit_data: BaseModel, kafka_topic: str, serializer_class: SerializerClass):
         logger.info(f"Auditing {audit_data} to {kafka_topic}")
         logger.debug(f"Current Thread: {threading.current_thread()}")
 
         # If the model is in the schema registry, we can just grab the schema from there, otherwise we need to generate
         # the schema and store it in the registry
-        if not isinstance(audit_data, AuditBaseModel):
-            raise ValueError("audit_data must be an instance of AuditBaseModel")
+        if not isinstance(audit_data, BaseModel):
+            raise ValueError("audit_data must be an instance of BaseModel")
 
         if audit_data.__class__.__name__ in self._schema_registry_models:
-            avro_serializer = self._schema_registry_models[audit_data.__class__.__name__]
+            serializer = self._schema_registry_models[audit_data.__class__.__name__]
         else:
-            avro_serializer = self._get_avro_serializer(audit_data)
-            self._schema_registry_models[audit_data.__class__.__name__] = avro_serializer
+            serializer = self._get_serializer(audit_data, serializer_class)
+            self._schema_registry_models[audit_data.__class__.__name__] = serializer
 
         try:
-            avro_value = avro_serializer(audit_data.avro_dict(), SerializationContext(kafka_topic, MessageField.VALUE))
+            serialized_value = serializer(audit_data.dict(), SerializationContext(kafka_topic, MessageField.VALUE))
         except SchemaRegistryError as e:
             self._produce_to_dead_letter_topic(audit_data.model_dump_json(), str(audit_data.audit_uuid))
             raise ValueError(
@@ -174,7 +165,7 @@ class Auditor:
         try:
             self._kafka_producer.produce(
                 topic=kafka_topic,
-                value=avro_value,
+                value=serialized_value,
                 key=str(audit_data.audit_uuid),
                 on_delivery=self._delivery_report,
             )
@@ -184,20 +175,37 @@ class Auditor:
         finally:
             self._kafka_producer.flush()
 
-    def _get_avro_serializer(self, model: AuditBaseModel) -> Optional[AvroSerializer]:
+    def _get_serializer(
+        self, model: BaseModel, serializer_class: SerializerClass
+    ) -> Optional[Union[AvroSerializer, JSONSerializer]]:
+        # TODO: explore switching to a cached_property for this method
+
         """Generate an Avro serializer for the provided model."""
         logger.debug(f"Current Thread: {threading.current_thread()}")
 
-        return (
-            AvroSerializer(
-                schema_registry_client=SchemaRegistryClient(
-                    conf={"url": self._schema_registry_url},
-                ),
-                schema_str=model.get_avro_schema(),
+        if not self._schema_registry_url:
+            logger.warning("No schema registry URL provided, NOT using schema registry for serialization.")
+            return None
+        else:
+            schema_registry_client = SchemaRegistryClient(
+                conf={"url": self._schema_registry_url},
             )
-            if self._schema_registry_url
-            else None
-        )
+
+        if serializer_class is SerializerClass.JSON:
+            serializer = JSONSerializer(
+                schema_registry_client=schema_registry_client,
+                schema_str=model.model_json_schema(),
+            )
+        elif serializer_class is SerializerClass.AVRO:
+            logger.warning("Avro serialization is experimental and may not work as expected.")
+            serializer = AvroSerializer(
+                schema_registry_client=schema_registry_client,
+                schema_str=py_avro_schema.generate(model.__class__).decode(),
+            )
+        else:
+            raise NotImplementedError(f"Serializer class {serializer_class} is not implemented.")
+
+        return serializer
 
     def _produce_to_retry_topic(self, value: str, key: str | None = None):
         """
@@ -288,7 +296,7 @@ class Auditor:
             raise Exception(f"A message failed to be delivered to the dead-letter topic: {err}")
 
 
-class HttpAuditRequest(AuditBaseModel):
+class HttpAuditRequest(BaseModel):
     """Audit request model."""
 
     uuid: UUID = Field(default_factory=uuid4)
@@ -311,7 +319,7 @@ class HttpAuditRequest(AuditBaseModel):
         return cls(**request_dict)
 
 
-class HttpAuditResponse(AuditBaseModel):
+class HttpAuditResponse(BaseModel):
     """Audit response model."""
 
     request_uuid: UUID
